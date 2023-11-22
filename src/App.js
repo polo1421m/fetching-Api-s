fb8c7782d6107fe30d48cff56432f0392207ad35
mrambabu <polo1421m@gmail.com>
2023-11-22 17:20:12 +0530

    changes dome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setRecords(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {records.map((list, index) => (
+          <li key={index}>
+            {list.id} {list.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
